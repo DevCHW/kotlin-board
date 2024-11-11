c877367cf3e357f8c6715df2c990a2f5de6873dc
DevCHW <ggoma003@naver.com>
2024-11-11 22:48:09 +0900

    게시글 생성 및 게시글 단건 조회 API 개발

--- src/main/kotlin/com/example/ktboard/api/v1/board/controller/BoardController.kt
+++ src/main/kotlin/com/example/ktboard/api/v1/board/controller/BoardController.kt
@@ -1,9 +1,12 @@
 package com.example.ktboard.api.v1.board.controller
 
 import com.example.ktboard.api.v1.board.controller.dto.request.CreateBoardRequest
+import com.example.ktboard.api.v1.board.controller.dto.response.BoardResponse
 import com.example.ktboard.api.v1.board.controller.dto.response.CreateBoardResponse
 import com.example.ktboard.domain.board.BoardService
 import com.example.ktboard.support.response.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -13,12 +16,25 @@ class BoardController(
     val boardService: BoardService,
 ) {
 
+    /**
+     * 게시글 생성
+     */
     @PostMapping("/api/v1/boards")
     fun createBoard(
         @RequestBody request: CreateBoardRequest,
     ): ApiResponse<CreateBoardResponse> {
         val result = boardService.create(request.toModel());
-        return ApiResponse.success(result)
+        return ApiResponse.success(CreateBoardResponse.from(result))
     }
 
+    /**
+     * 게시글 단건 조회
+     */
+    @GetMapping("/api/v1/boards/{boardId}")
+    fun getBoard(
+        @PathVariable boardId: Long
+    ): ApiResponse<BoardResponse> {
+        val result = boardService.findById(boardId)
+        return ApiResponse.success(BoardResponse.from(result))
+    }
 }
--- src/main/kotlin/com/example/ktboard/api/v1/board/controller/dto/response/BoardResponse.kt
+++ src/main/kotlin/com/example/ktboard/api/v1/board/controller/dto/response/BoardResponse.kt
@@ -3,17 +3,16 @@ package com.example.ktboard.api.v1.board.controller.dto.response
 import com.example.ktboard.domain.board.model.Board
 import java.time.LocalDateTime
 
-data class CreateBoardResponse(
+data class BoardResponse private constructor(
     val id: Long,
     val title: String,
     val content: String,
     val createdAt: LocalDateTime,
     val updatedAt: LocalDateTime,
 ) {
-
     companion object {
-        fun from(board: Board): CreateBoardResponse {
-            return CreateBoardResponse(
+        fun from(board: Board): BoardResponse {
+            return BoardResponse(
                 id = board.id,
                 title = board.title,
                 content = board.content,
